55f21e048997746b0d03eb072490723e231fcb7e
Ratiel Alves <ratielalves8@gmail.com>
2021-02-20 03:26:40 -0300

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ export.handler = event => {
  		'content-type': "application/json",
  		'auth-key': "api_key"		//Insert you api key for authentication
  	},
- 	body JSON.stringify({
+ 	body JSON.stringify:({
  		Sender: 'lambda',
  		Receivers: 'PhoneNumber'	//Insert the Phone Number from receiver
  		Content: 'Servidor esta com capacidade limitada'  //Message sent to user
